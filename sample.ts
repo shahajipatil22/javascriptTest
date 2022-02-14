04be05c91c8e86cac89370b138fcb2ecc51e3e56
CoderGeek <shahajipatil22@gmail.com>
2022-02-14 12:56:53 +0530

    Update sample.ts

--- sample.ts
+++ sample.ts
@@ -9,3 +9,36 @@ export default () => {
     });
   }
 };
+
+
+try {
+  eval(code);
+} catch (e) {
+  alert(e);
+}
+
+function randomString() {
+    var gencode = document.getElementById('gencode'),
+        length = document.getElementById('length').value,
+        chars = document.getElementById('chars').value,
+        mask = '',
+        result = '';
+
+    console.log(length, chars);
+
+    if (chars.indexOf('a') > -1) mask += 'abcdefghijklmnopqrstuvwxyz';
+    if (chars.indexOf('A') > -1) mask += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (chars.indexOf('#') > -1) mask += '0123456789';
+
+    for (var i = length; i > 0; --i) {
+        result += mask[Math.round(Math.random() * (mask.length - 1))];
+    }
+
+    console.log(result);
+
+    gencode.value = result;
+}
+
+window.addEventListener('load', function load(){
+    document.getElementById('generate').addEventListener('click', randomString);
+});
