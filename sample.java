ba6a0ebfa12efc4b9548869c4eb2a1828ff2f9f4
CoderGeek <shahajipatil22@gmail.com>
2022-02-14 15:25:46 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,2 +1,13 @@
-System.out.println("");
+// Java code to illustrate
+// System.out.println();
 
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome");
+		System.out.println("To");
+		System.out.println("GeeksforGeeks");
+	}
+}
