0125c03f51dc880ded2f4abf3659d3d0bc7be513
CoderGeek <shahajipatil22@gmail.com>
2022-02-14 18:21:51 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,45 +1,4 @@
-let f: (() => void) | null = null
+let a =5;
+let x =10;
 
-function g() {
-  f = () => {}
-}
-
-g()
-
-if (f) {
-  f() // <- Error: "This expression is not callable 
-      //            Type 'never' has no call signatures. (2349)"
-}var codeJS = null;
-
-console.log(codeJS);
-      //output is null
-
-console.log(typeof codeJS);
-     //output is object
-console.log(null==undefined);
-//output is true
-
-console.log(null===undefined);
-//output is false
-
-var coder = {
-      name: "Peter",
-      age: 27,
-      speak() {
-      console.log(this.name);
-  }
-};
-coder.speakNow();
-
-function number(n) {
-    var add = 5/9;
-    return;
-    n + add;
-     }
-
-for(i;i<10){
-console.log(number(10));
-}
-
-
-System.out.println("sjhahaji");
+console.log("hello world");
