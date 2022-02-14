cc0242d81efa16633bc3acd44f80c8c533fb417c
CoderGeek <shahajipatil22@gmail.com>
2022-02-14 12:54:19 +0530

    Update sample.ts

--- sample.ts
+++ sample.ts
@@ -1,6 +1,11 @@
-step = 0;
-
-    searchValue = '';
-    filters = new FormControl();
-    months: year[] = [
-        { value: '1', viewValue: 'Jan
+export default () => {
+  if (process.env.NODE_ENV !== 'development' && 'serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+        console.log('SW registered: ', registration);
+      }).catch((registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      });
+    });
+  }
+};
