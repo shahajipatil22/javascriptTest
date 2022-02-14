37b7c72539eeedf1b83b6f0aa9b5f7928f75b7d4
CoderGeek <shahajipatil22@gmail.com>
2022-02-14 13:02:28 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -10,3 +10,14 @@ if (f) {
   f() // <- Error: "This expression is not callable 
       //            Type 'never' has no call signatures. (2349)"
 }
+ function renderOptionData() {
+            $('.plugin_option_list .option_wrapper').html(Template.suppressStatusOptions({
+                'issues_options': i18next.t('issues.issues_options'),
+                'issue_types': i18next.t('issues.issue_types')
+            }));
+            $('.options_holder').addClass('suppress_status_panel_options');
+
+            $('.parameter_wrapper .parameter_inner_sub_wrapper .issue-options-wrapper').html(Template.suppressStatusFilters({
+
+            }));
+        }
