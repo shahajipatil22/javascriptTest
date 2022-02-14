341f94f74a907ca2f12b2d2b462af1e3740c8846
CoderGeek <shahajipatil22@gmail.com>
2022-02-14 12:58:32 +0530

    Update sample.ts

--- sample.ts
+++ sample.ts
@@ -1,44 +1,12 @@
-export default () => {
-  if (process.env.NODE_ENV !== 'development' && 'serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js').then((registration) => {
-        console.log('SW registered: ', registration);
-      }).catch((registrationError) => {
-        console.log('SW registration failed: ', registrationError);
-      });
-    });
-  }
-};
+let f: (() => void) | null = null
 
-
-try {
-  eval(code);
-} catch (e) {
-  alert(e);
+function g() {
+  f = () => {}
 }
 
-function randomString() {
-    var gencode = document.getElementById('gencode'),
-        length = document.getElementById('length').value,
-        chars = document.getElementById('chars').value,
-        mask = '',
-        result = '';
+g()
 
-    console.log(length, chars);
-
-    if (chars.indexOf('a') > -1) mask += 'abcdefghijklmnopqrstuvwxyz';
-    if (chars.indexOf('A') > -1) mask += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    if (chars.indexOf('#') > -1) mask += '0123456789';
-
-    for (var i = length; i > 0; --i) {
-        result += mask[Math.round(Math.random() * (mask.length - 1))];
-    }
-
-    console.log(result);
-
-    gencode.value = result;
+if (f) {
+  f() // <- Error: "This expression is not callable 
+      //            Type 'never' has no call signatures. (2349)"
 }
-
-window.addEventListener('load', function load(){
-    document.getElementById('generate').addEventListener('click', randomString);
-});
